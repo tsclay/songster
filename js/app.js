e8303322e7a8bd03fd85459c3a67a30bce1452ef
Tim <tsclay9@gmail.com>
2020-05-15 20:14:35 -0400

    added AJAX for lyrics and artist data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// AJAX call for lyrics to the song; user must input artist and song (using Lyrics.ovh)
 $.ajax({
-  url: 'https://api.lyrics.ovh/v1/Tycho/H E R'
+  url: 'https://api.lyrics.ovh/v1/Tycho/"Pink & Blue (feat. Saint Sinner)"'
 }).then(
   (data) => {
     console.log(data)
@@ -11,3 +12,16 @@ $.ajax({
 
 // SongMeanings API:
 // http://developer.echonest.com/api/v4/song
+
+// AJAX call for artist info and album info for the track requested (using Deezer)
+$.ajax({
+  url:
+    'https://api.deezer.com/search?q=track:%22Rosyln%22%20artist:%22Bon%20Iver%22'
+}).then(
+  (data) => {
+    console.log(data)
+  },
+  (error) => {
+    console.log(error)
+  }
+)
