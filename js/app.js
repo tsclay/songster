e85d670be25ef6876be2dc3fa40407a78b0e9292
Tim <tsclay9@gmail.com>
2020-05-15 23:23:37 -0400

    can get songs from general query and search for a specific song in order to get larger span of data for song

--- js/app.js
+++ js/app.js
@@ -30,6 +30,10 @@
 // Link to help for Genius API: https://www.youtube.com/watch?v=-TgXQQQdzWY
 const query = 'Pink & Blue by Tycho'
 const token = 'OxYfGhNsuLpSKms2y3EW7jrnIY21V5Yg6GyrOthWokYbDI5k280rvP-KTpTbNwVq'
+const token2 =
+  'glhXJYWphrHh8Xnoev9a8YKCm4hd78J16RXN-jF0p2YOkBAhTLcZLKp4l47isNNo'
+let songID = 0
+
 $.ajax({
   url: `https://api.genius.com/search?access_token=${token}&q=${encodeURIComponent(
     query
@@ -38,11 +42,44 @@ $.ajax({
   (data) => {
     // Song title followed with artist and featured, if applies
     console.log(data.response.hits[0].result.full_title)
+    // Song ID in Genius to ping the song directly for deeper info
+    songID = data.response.hits[0].result.id
+    // Help for the 'settings' object preceding the then-callback is brought to me in part by RapidApi and their awesome code snippets (not sponsored)
+    $.ajax({
+      url: `https://cors-anywhere.herokuapp.com/https://api.genius.com/songs/${songID}`,
+      crossDomain: true,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(
+      (song) => {
+        console.log(song)
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
   },
   (error) => {
     console.log(error)
   }
 )
+
+// songID = 4552408
+// $.ajax({
+//   url: `https://cors-anywhere.herokuapp.com/https://api.genius.com/songs/${songID}`,
+//   crossDomain: true,
+//   headers: {
+//     Authorization: `Bearer ${token}`
+//   }
+// }).then(
+//   (data) => {
+//     console.log(data)
+//   },
+//   (error) => {
+//     console.log(error)
+//   }
+// )
 
 // $.ajax({
 //   url:
