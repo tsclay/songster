ed66ec3a35a75b5df2800da4eae302293ed96f86
Tim <tsclay9@gmail.com>
2020-05-16 22:37:14 -0400

    specified classes of jQuery objects

--- js/app.js
+++ js/app.js
@@ -7,9 +7,10 @@
 // Link to help for Genius API: https://www.youtube.com/watch?v=-TgXQQQdzWY
 const token = 'OxYfGhNsuLpSKms2y3EW7jrnIY21V5Yg6GyrOthWokYbDI5k280rvP-KTpTbNwVq'
 let songID = 0
-const songLyrics = ''
+let songLyrics = ''
 
 $('form').on('submit', (event) => {
+  $('.container').empty()
   event.preventDefault()
   const query = $('input').val()
   console.log(query)
@@ -25,11 +26,13 @@ $('form').on('submit', (event) => {
       console.log(data.response.hits)
 
       for (let i = 0; i <= data.response.hits.length; i++) {
+        songID = data.response.hits[i].result.id
+        const title = data.response.hits[i].result.full_title
         const hit = $('<div>').addClass('general-search-result')
-        const songAndArtist = $('<p>').text(
-          data.response.hits[i].result.full_title
-        )
-        const albumArt = $('<img>').attr(
+        const songAndArtist = $('<a class="genius-song-link">')
+          .text(title)
+          .attr('song-id', `${songID}`)
+        const albumArt = $('<img class="album-art">').attr(
           'src',
           data.response.hits[i].result.header_image_url
         )
@@ -39,47 +42,48 @@ $('form').on('submit', (event) => {
       // console.log(data.response.hits)
       // console.log(data.response.hits[0].result.full_title)
       // Song ID in Genius to ping the song directly for deeper info
-      songID = data.response.hits[0].result.id
+
       // Help for the 'settings' object preceding the then-callback is brought to me in part by RapidApi and their awesome code snippets (not sponsored)
-      // $.ajax({
-      //   url: `https://cors-anywhere.herokuapp.com/https://api.genius.com/songs/${songID}`,
-      //   crossDomain: true,
-      //   headers: {
-      //     Authorization: `Bearer ${token}`
-      //   }
-      // }).then(
-      //   (song) => {
-      //     // console.log(song)
-      //     songLyrics = song.response.song.url
-      //     $.ajax({
-      //       url: `https://cors-anywhere.herokuapp.com/${songLyrics}`,
-      //       crossDomain: true,
-      //       headers: {
-      //         Authorization: `Bearer ${token}`
-      //       }
-      //     }).then(
-      //       (data) => {
-      //         // console.log('Lyrics data', data)
-      //         // const $lyrics = $.parseHTML(data, null, false)
-      //         // console.log($lyrics.find($('routable-page')))
-      //         // $(data).find()
-      //         let $search = $(data).children().eq(6).text()
-      //         $search = $search.slice(
-      //           $search.indexOf('Duality Lyrics'),
-      //           $search.indexOf('More on Genius')
-      //         )
-      //         console.log($search)
-      //         // console.log($htmlData.children())
-      //       },
-      //       (error) => {
-      //         console.log('Lyrics error', error)
-      //       }
-      //     )
-      //   },
-      //   (error) => {
-      //     console.log('Song/ID error:', error)
-      //   }
-      // )
+      $.ajax({
+        url: `https://cors-anywhere.herokuapp.com/https://api.genius.com/songs/${songID}`,
+        crossDomain: true,
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then(
+        (song) => {
+          // console.log(song)
+          songLyrics = song.response.song.url
+          $.ajax({
+            url: `https://cors-anywhere.herokuapp.com/${songLyrics}`,
+            crossDomain: true,
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }).then(
+            (lyrics) => {
+              // console.log('Lyrics data', data)
+              // const $lyrics = $.parseHTML(data, null, false)
+              // console.log($lyrics.find($('routable-page')))
+              // $(data).find()
+              let $search = $(lyrics).children().eq(6).text()
+              $search = $search.slice(
+                $search.indexOf('Skinny Love Lyrics'),
+                $search.indexOf('More on Genius')
+              )
+              console.log($search, 'asdfasdfasdf')
+              console.log('are we seeing thiis')
+              // console.log($htmlData.children())
+            },
+            (error) => {
+              console.log('Lyrics error', error)
+            }
+          )
+        },
+        (error) => {
+          console.log('Song/ID error:', error)
+        }
+      )
     },
     (error) => {
       console.log('General search error', error)
