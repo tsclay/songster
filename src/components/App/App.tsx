a89f0b2c1ba7cfe2f5e4861f457b8ec69d87f41f
Tim Clay <tsclay9@gmail.com>
2021-01-17 00:23:24 -0500

    working on component tree

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchResults from '../Content/SearchResults';
+import SearchBar from '../Header/SearchBar';
+import Lyrics from '../Content/Lyrics';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="Header">
+        <h1>Songster</h1>
+        <SearchBar />
+      </nav>
+      <div className="Content">
+        <SearchResults />
+        <Lyrics />
+      </div>
     </div>
   );
 }
