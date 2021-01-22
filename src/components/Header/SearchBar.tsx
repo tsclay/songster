0c18fa0445ce6496413cc2cb1b1a332132a4a2f2
Tim <tsclay9@gmail.com>
2021-01-22 11:13:20 -0500

    add styling to SearchBar

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+import './SearchBar.scss';
 
 interface Props {
   inputRef: React.MutableRefObject<string>;
-  handleSearch: () => void;
+  handleSearch: (e: React.SyntheticEvent) => void;
 }
 
 export const SearchBar: React.FC<Props> = (props) => {
   const { inputRef, handleSearch } = props;
+  const underline = useRef<HTMLDivElement>(null);
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputElement && inputElement.current) {
+      inputElement.current.focus();
+    }
+  });
   return (
-    <div>
+    <div className="SearchBar">
       <input
+        ref={inputElement}
+        onFocus={() => {
+          if (underline && underline.current) {
+            underline.current.className = 'input-underline focused';
+          }
+        }}
+        onBlur={() => {
+          if (underline && underline.current) {
+            underline.current.className = 'input-underline';
+          }
+        }}
+        form="search-genius"
         onChange={(e) => {
           inputRef.current = e.target.value;
         }}
         type="text"
       />
-      <button type="button" onClick={handleSearch}>
+      <div ref={underline} className="input-underline"></div>
+      <button form="search-genius" type="button">
         🔎
       </button>
+      <form
+        id="search-genius"
+        onSubmit={(e) => {
+          handleSearch(e);
+        }}
+      ></form>
     </div>
   );
 };
