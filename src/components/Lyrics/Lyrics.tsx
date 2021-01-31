38e6737b4fba6ef5c64cd70fef0199fc5ebbd609
Tim Clay <tsclay9@gmail.com>
2021-01-31 11:27:18 -0500

    setup mechanism for grabbing extra metadata about selected song

--- src/components/Lyrics/Lyrics.tsx
+++ src/components/Lyrics/Lyrics.tsx
@@ -12,17 +12,39 @@ interface Props {
 export const Lyrics: React.FC<Props> = (props) => {
   const { url, contentReducer, prevSearchTerm } = props;
   const [gotLyrics, setGotLyrics] = useState(false);
+  // const [songMetadata, setSongMetadata] = useState(null);
   const lyricContent = useRef('');
   const aboutContent = useRef('');
+  const infoDiv = useRef<null | HTMLDivElement>(null);
+
+  // const fetchSongData = useCallback(
+  //   async (url: string | null): Promise<any> => {
+  //     if (!url) return;
+  //     const response = await fetch(url, {
+  //       method: 'GET',
+  //       mode: 'cors',
+  //       headers: {
+  //         Accept: 'application/json'
+  //       }
+  //     });
+  //     if (response) {
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setSongMetadata(data);
+  //     }
+  //   },
+  //   []
+  // );
+
   const fetchLyrics = useCallback(async (url: string | null): Promise<any> => {
     if (!url) return;
     // console.log(url);
-    const response = await fetch(`http://0.0.0.0:8080/${url}`, {
+    const response = await fetch(`http://0.0.0.0:7000/${url}`, {
       method: 'GET',
       mode: 'cors',
       headers: {
-        Accept: 'application/json',
-      },
+        Accept: 'application/json'
+      }
     });
     // console.log(response);
     if (response) {
@@ -42,7 +64,7 @@ export const Lyrics: React.FC<Props> = (props) => {
       .trimLeft();
     const foundInfo = template.content
       .querySelector('div.rich_text_formatting')
-      ?.textContent?.trimEnd()
+      ?.innerHTML?.trimEnd()
       .trimLeft();
     lyricContent.current = foundLyrics || '';
     aboutContent.current = foundInfo || '';
@@ -51,7 +73,7 @@ export const Lyrics: React.FC<Props> = (props) => {
   const makeMarkup = (htmlString: string): { __html: string } | undefined => {
     return htmlString
       ? {
-          __html: htmlString,
+          __html: htmlString
         }
       : undefined;
   };
@@ -60,32 +82,54 @@ export const Lyrics: React.FC<Props> = (props) => {
     fetchLyrics(url);
   }, [url, fetchLyrics]);
 
+  // useEffect(() => {
+  //   fetchSongData(null);
+  // }, [fetchSongData]);
+
   return (
     <div className="Lyrics">
-      <button
-        className="return-btn"
-        onClick={(e) => {
-          contentReducer({
-            type: 'SEARCHING',
-            searchTerm: prevSearchTerm,
-            urlToLyrics: null,
-          });
-        }}
-      >
-        ⬅
-      </button>
-      <p>What up lyrics</p>
+      <div className="user-options">
+        <button
+          className="return-btn"
+          type="button"
+          onClick={(e) => {
+            contentReducer({
+              type: 'SEARCHING',
+              searchTerm: prevSearchTerm,
+              urlToLyrics: null
+            });
+          }}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z" />
+          </svg>
+        </button>
+        <div>
+          <button
+            type="button"
+            onClick={(e) => {
+              infoDiv.current?.classList.toggle('show');
+            }}
+          >
+            info
+          </button>
+          <button type="button">video</button>
+        </div>
+      </div>
       {gotLyrics ? (
-        <>
+        <div className="lyrics-body">
+          <p>This song</p>
           <div
-            className="song-lyrics"
+            className="song-lyrics dynamic-font-size"
             dangerouslySetInnerHTML={makeMarkup(lyricContent.current)}
           ></div>
           <div
-            className="song-about"
+            ref={infoDiv}
+            className="song-info dynamic-font-size"
             dangerouslySetInnerHTML={makeMarkup(aboutContent.current)}
           ></div>
-        </>
+          {/* <div>{JSON.stringify(songMetadata)}</div> */}
+        </div>
       ) : (
         <h2>Waiting...</h2>
       )}
